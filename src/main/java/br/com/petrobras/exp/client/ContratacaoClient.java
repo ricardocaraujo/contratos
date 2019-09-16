840fa83909f61f0b578baa5cb82855df33b0d51d
ricardo <rcavaraujo@gmail.com>
2019-09-15 23:38:03 -0300

    Continuando desenvolvimento de ContratacaoClient

--- src/main/java/br/com/petrobras/exp/client/ContratacaoClient.java
+++ src/main/java/br/com/petrobras/exp/client/ContratacaoClient.java
@@ -2,7 +2,12 @@ package br.com.petrobras.exp.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.petrobras.exp.modelo.Contratacao;
 
@@ -18,7 +23,9 @@ public class ContratacaoClient {
 	}
 	
 	public void adicionaContratacao(Contratacao contratacao) {
-		
+		String xml = new XStream().toXML(contratacao);
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		Response resposta = webtarget.path("/rest/contratacao").request().post(entity);		
 	}
 	
 
